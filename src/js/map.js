5ae61fc44d0604342f85e89e2b2c47ef81894b8f
OlenaLotos <olenameshanych@gmail.com>
2022-09-09 15:21:16 +0300

    some changes

--- src/js/map.js
+++
@@ -1,7 +0,0 @@
-       function myMap() {
-          var mapProp = {
-            center: new google.maps.LatLng(49.841321, 24.031300),
-            zoom: 10,
-          };
-          var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-        }
