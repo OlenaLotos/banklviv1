265d7a6941035488787a43d80625f7a730ac39b5
OlenaLotos <94827284+OlenaLotos@users.noreply.github.com>
2022-09-09 12:38:54 +0000

    Deploying to gh-pages from @ OlenaLotos/banklviv1@a91a16e9471ec89a76d8971ae676af33b8c07479 🚀

--- src.456c153d.js
+++ src.456c153d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/top.png":[["top.40d70c01.png","FR3c"],"FR3c"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/baklviv1/src.97780afd.js.map
+//# sourceMappingURL=/baklviv1/src.456c153d.js.map
